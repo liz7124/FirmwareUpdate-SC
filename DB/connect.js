ba61d66890239e6ae152b5f37467be5b38af9694
Elizabeth Nathania W <liza_1805@hotmail.com>
2020-04-16 16:12:06 +0900

    Update connect.js

--- DB/connect.js
+++ DB/connect.js
@@ -1,10 +1,11 @@
 var mysql = require('mysql');
+var util = require('util');
 
 const DBname = "firmware_update";
 const metadata_table = "firmware_update_metadata";
 const device_table = "device_data";
 const firmware_update_transactions_table = "firmware_update_transactions";
-var con;
+//var con;
 
 var self = module.exports = {
 
@@ -16,10 +17,20 @@ var self = module.exports = {
             database: DBname
         });
 
-        con.connect(function(err) {
+        /*con.connect(function(err) {
             if (err) throw err;
             console.log("Connected!");
-        });
+        });*/
+
+        return {
+            query(sql, args) {
+                return util.promisify(con.query)
+                .call(con,sql,args);
+            },
+            close() {
+                return util.promisify(con.end).call(con);
+            }
+        };
     },
 
     //insert to device table
@@ -29,6 +40,21 @@ var self = module.exports = {
             if (err) throw err;
             console.log("1 record inserted");
         });
+    },
+
+    //check if data exist
+    checkDeviceExist: async function(Mid,uri) {
+        var con = this.connectDB();
+        var sql = "SELECT COUNT(*) as count FROM " + device_table + " WHERE manufacturer_id=" + Mid + " and uri='" + uri + "';";
+        const result = await con.query(sql);
+        return result[0]['count'];
+        
+        /*
+        con.query(sql, function (err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+            return result;
+        });*/
     },
 
     //insert to firmware update transactions table
@@ -41,21 +67,44 @@ var self = module.exports = {
     },
 
     //insert metadata to metadata table
-    saveMetadataFirmwareUpdate: function(hash,manufacturer_id,firmware_version,dtype,release_date,url) {
-        var sql = "INSERT INTO " + metadata_table + " (hash,manufacturer_id,firmware_version,device_type,release_date,url) VALUES ('"+hash+"',"+manufacturer_id+","+firmware_version+",'"+dtype+"','"+release_date+"','"+url+"');";
+    saveMetadataFirmwareUpdate: function(hash,manufacturer_id,firmware_version,dtype,release_date,url,signature) {
+        var sql = "INSERT INTO " + metadata_table + " (hash,manufacturer_id,firmware_version,device_type,release_date,url,signature) VALUES ('"+hash+"',"+manufacturer_id+","+firmware_version+",'"+dtype+"','"+release_date+"','"+url+"','"+signature+"');";
         con.query(sql, function (err, result) {
             if (err) throw err;
             console.log("1 record inserted");
         });
     },
 
-    //check if data exist
-    checkDeviceExist: function(Mid,uri) {
-        var sql = "SELECT COUNT(*) FROM " + device_table + "WHERE manufacturer_id=" + Mid + " and uri='" + uri + "';";
+    //check FU metadata compatibility
+    checkMetadata: async function(Mid, dtype, fv) {
+        var con = this.connectDB();
+        var sql = "SELECT * FROM " + metadata_table + " WHERE manufacturer_id=" + Mid + " and device_type like '%" + dtype + "%' and firmware_version > " + fv + ";";
+        const result = await con.query(sql);
+        return result;
+        /*
         con.query(sql, function (err, result, fields) {
             if (err) throw err;
             console.log(result);
             return result;
-        });
+        });*/
+    },
+
+    //check metadata exist
+    checkMetadataExist: async function(Uid) {
+        var con = this.connectDB();
+        var sql = "SELECT COUNT(*) as count FROM " + metadata_table + " WHERE hash='" + Uid + "';";
+        const result = await con.query(sql);
+        //console.log(result);
+        return result[0]['count'];
+
+        /*con.query(sql, function (err, result, fields) {
+            if (err) {throw err;}
+            //console.log(result[0]['count']);
+            res = result[0]['count'];
+            console.log(res);
+            return res;
+        });*/
     }
+
+    
 }
